18eacc51d33e39bdef82d36108474658ee227edf
Toby-Bear <bimsetiger@gmail.com>
2023-03-17 08:45:55 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 let images=[]
-let tiles =[]
+const tiles =[]
+let puzzles
 
 function preload(){
-for (p=0;p<16;p++){
-  let picture = loadImage("images/img"+p+".png")
-  images.push(picture)
+for (p=0;p<4;p++){
+  for (y=0;y<4;y++){
+    let image = loadImage("images/img"+p+".png")
+  images.push(image)
+  tiles.push(new tile(windowWidth/4*p,windowHeight/4*y,images[p],windowWidth,windowHeight))  
+  }
 }
 }
 function setup() {
@@ -14,7 +18,9 @@ function setup() {
 
 function draw() {
   background(220);
-  for(let tile of tiles)
-  tile.display()
+  for(let tile of tiles){
+    tile.display()
+
+  }
 }
 
